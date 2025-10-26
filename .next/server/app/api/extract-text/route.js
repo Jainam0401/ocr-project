1e6856985f7879d38c90f5c5f681ff0f81222afe
jainam0401 <jainammithalaljain@gmail.com>
2025-10-26 19:16:20 +0530

    corrected typescript issuse

---
+++ .next/server/app/api/extract-text/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/extract-text/route.js")
+R.c("server/chunks/[root-of-the-server]__3ef367df._.js")
+R.c("server/chunks/[root-of-the-server]__b2da729a._.js")
+R.c("server/chunks/[root-of-the-server]__fe75acf3._.js")
+R.c("server/chunks/_next-internal_server_app_api_extract-text_route_actions_c14d2285.js")
+R.m(64792)
+module.exports=R.m(64792).exports
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"T0RcoXfTQVl286L+3qegsNmyTIn18oqk5gBG92RJjZI=\"\n}"
